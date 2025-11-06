706055124e881d62b79a0b1b2276e7afad46bcc4
VeryCreativeName64 <marekbendeguz@gmail.com>
2025-11-06 13:02:08 +0100

    AjtoModell.java konstruktor es getter

--- src/modell/AjtoModell.java
+++ src/modell/AjtoModell.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modell;
 
-/**
- *
- * @author marek.b.kristof
- */
+
 public class AjtoModell {
+    private String szin;
+    private boolean auto;
+
+    public AjtoModell(String szin, boolean auto) {
+        this.szin = szin;
+        this.auto = auto;
+    }
+
+    public String getSzin() {
+        return szin;
+    }
+
+    public boolean isAuto() {
+        return auto;
+    }
+
+    
+    
+    
+
+    
+
+    
     
 }
